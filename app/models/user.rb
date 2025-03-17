6b8c66d23bf30edecc3d7db8b254151dfd5f92be
Ubuntu <jd069@hotmail.com>
2025-03-17 14:46:21 +0000

    Readded users table with uuids

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  enum role: [:student, :teacher, :admin]
+         
+  validates_presence_of :first_name, :last_name, :email, :role
+  
+  def name
+    "#{first_name} #{last_name}"
+  end
+  
+  def initials
+    "#{first_name.first}#{last_name.first}".upcase 
+  end
 end
--- db/migrate/20250316193456_devise_create_users.rb
+++ db/migrate/20250316193456_devise_create_users.rb
@@ -2,7 +2,7 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :users do |t|
+    create_table :users, id: :uuid do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -26,20 +26,23 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
+      
+      t.integer :role, default: 0
+      t.string :first_name, null: false
+      t.string :last_name, null: false
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-      
-      t.integer :role, default: 0
+
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
