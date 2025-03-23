a594460a2e4bafa10ee075c58e94b1d21a65caf5
Ubuntu <jd069@hotmail.com>
2025-03-23 19:09:54 +0000

    Edited user edit page and layout file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_admin
+  before_action :require_admin, except: [:show, :admin_edit]
   
   def index
     @users = User.all
