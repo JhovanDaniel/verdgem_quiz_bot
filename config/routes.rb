6d95ff149a7353aeecf662b79a7f277dbeb0f04a
Ubuntu <jd069@hotmail.com>
2025-03-26 03:23:15 +0000

    Added contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -64,6 +64,7 @@ Rails.application.routes.draw do
   get 'pages/our_team'
   get 'pages/our_blog'
   get 'pages/csec_quizzes'
+  get 'pages/contact_us'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
