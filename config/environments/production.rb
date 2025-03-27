71fcd97ac0e1e436c7cd0579e7249eb2f54912a3
Ubuntu <jd069@hotmail.com>
2025-03-27 19:11:07 +0000

    Added production email configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -99,18 +98,19 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://quiz.verdgem.com' }  
   #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
     
   #~~~~~~~~~~~~~~~~ OUTLOOK CONFIG ~~~~~~~~~~~~~~~~~~~~#
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp-mail.outlook.com',
+    address:              'smtp.titan.email',
     port:                 587,
-    domain:               'quiz.verdgem.com', # Your domain or any domain you have access to
-    user_name:            ENV['TEST_EMAIL'], # Your Outlook email address
-    password:             ENV['TEST_EMAIL_PASS'], # Your Outlook email password
-    authentication:       :login,
-    enable_starttls_auto: true 
+    domain:               'quiz.verdgem.com',
+    user_name:            ENV['TEST_EMAIL'],
+    password:             ENV['TEST_EMAIL_PASS'], 
+    authentication:       :login, 
+    enable_starttls_auto: true
   }
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
