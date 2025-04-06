e69513e827081ecf0d21d6bf4d4169d9107bddf2
Ubuntu <jd069@hotmail.com>
2025-04-06 01:09:25 +0000

    Added country to emoji to app helper

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import 'flag-icon-css/css/flag-icons.min.css'
 import * as bootstrap from "bootstrap"
