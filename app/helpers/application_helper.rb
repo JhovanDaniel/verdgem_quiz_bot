9c7553c8402a97d274640ec9151e1cf1bee549e0
Ubuntu <jd069@hotmail.com>
2025-04-06 00:32:07 +0000

    Added country to emoji to app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
       date.strftime("%B %-d, %Y")
     end
   end
+  
+  def country_code_to_emoji(country_code)
+    # Convert 2-letter country code to flag emoji
+    # Country code characters are converted to regional indicator symbols
+    country_code.upcase.chars.map { |c| (c.ord + 127397).chr(Encoding::UTF_8) }.join
+  end
 end
