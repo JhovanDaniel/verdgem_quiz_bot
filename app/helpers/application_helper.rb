7cfa5fa647c10a1a172f7abe070f9bb449d420e0
Ubuntu <jd069@hotmail.com>
2025-04-06 01:38:01 +0000

    Added country to emoji to app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,11 @@ module ApplicationHelper
   end
   
   def country_code_to_emoji(country_code)
+    return "" if country_code.blank?
+    
     # Convert 2-letter country code to flag emoji
-    # Country code characters are converted to regional indicator symbols
+    # Each country code character is converted to a regional indicator symbol
+    # (offset by 127397 from the ASCII value)
     country_code.upcase.chars.map { |c| (c.ord + 127397).chr(Encoding::UTF_8) }.join
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import 'flag-icon-css/css/flag-icons.min.css'
 import * as bootstrap from "bootstrap"
