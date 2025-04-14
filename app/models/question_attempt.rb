0dd73b2e54019e290e41c63d13f2168de15a2d7d
Ubuntu <jd069@hotmail.com>
2025-04-14 01:12:28 +0000

    Added random character expressions to questions

--- app/models/question_attempt.rb
+++ app/models/question_attempt.rb
@@ -14,4 +14,27 @@ class QuestionAttempt < ApplicationRecord
     question.max_points
   end
   
+  def self.question_character_image
+    character_images = {
+      "character_expressions/jade-happy.png" => "Jade Happy",
+      "character_expressions/jade-excited.png" => "Jade Excited",
+      "character_expressions/jade-party.png" => "Jade Party",
+      "character_expressions/jade-shocked.png" => "Jade Shocked",
+      "character_expressions/emeri-happy.png" => "Emeri Happy",
+      "character_expressions/emeri-excited.png" => "Emeri Excited",
+      "character_expressions/emeri-party.png" => "Emeri Party",
+      "character_expressions/emeri-shocked.png" => "Emeri Shocked",
+      "character_expressions/peri-happy.png" => "Peri Happy",
+      "character_expressions/peri-excited.png" => "Peri Excited",
+      "character_expressions/peri-party.png" => "Peri Party",
+      "character_expressions/peri-shocked.png" => "Peri Shocked",
+      "character_expressions/jasper-happy.png" => "Jasper Happy",
+      "character_expressions/jasper-excited.png" => "Jasper Excited",
+      "character_expressions/jasper-party.png" => "Jasper Party",
+      "character_expressions/jasper-shocked.png" => "Jasper Shocked",
+    }
+    
+    character_images.to_a.sample
+  end
+    
 end
