ce341406c0588a6b098176219e1478385fbc9890
Ubuntu <jd069@hotmail.com>
2025-05-21 16:43:46 +0000

    Added subtopic to question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -83,7 +83,7 @@ class QuestionsController < ApplicationController
   
   def question_params
     params.require(:question).permit(:content, :model_answer, :key_concepts, :marking_criteria, 
-      :max_points, :difficulty_level, :has_problem, :question_type,
+      :max_points, :difficulty_level, :has_problem, :question_type, :sub_topic_id,
       answer_options_attributes: [:id, :content, :is_correct, :position, :_destroy]
     )
   end
