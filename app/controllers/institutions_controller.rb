28e4c65f632f32b0f98d159787cf1cd070d3050e
Ubuntu <jd069@hotmail.com>
2025-06-06 01:43:02 +0000

    Added institution edit and show actions and views

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -1,5 +1,6 @@
 class InstitutionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_institution, only: [:edit, :update, :show]
 
   def index
     @institutions = Institution.all
@@ -9,21 +10,39 @@ class InstitutionsController < ApplicationController
   end
   
   def new
+    @institution = Institution.new
   end
   
   def create 
+    @institution = Institution.new(institution_params)
+    
+    if @institution.save
+      redirect_to institution_path(@institution), notice: 'Institution was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   def edit
+    
   end
   
   def update
+    if @institution.update(institution_params)
+      redirect_to institution_path(@institution), notice: 'Institution was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
   
   private
   
-  def institutions_params
-    params.require(:subject).permit(:name, :description, :active, :contact_email, :phone, :address)
+  def set_institution
+    @institution = Institution.find(params[:id])
+  end
+  
+  def institution_params
+    params.require(:institution).permit(:name, :description, :active, :contact_email, :phone, :address)
   end
   
 
--- app/models/institution.rb
+++ app/models/institution.rb
@@ -2,6 +2,19 @@ class Institution < ApplicationRecord
   has_many :users, dependent: :nullify
   
   validates :name, presence: true, uniqueness: true
+  validates :contact_email, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
 
   scope :active, -> { where(active: true) }
+  
+  def status
+    if active == true
+      institution_status = "<span class='badge badge-primary rounded-lg mx-2'>●</span><span class=''>Active</span>"
+      institution_status.html_safe
+    else
+      institution_status = "<span class='badge badge-danger rounded-lg mx-2'>●</span><span class=''>Inactive</span>"
+      institution_status.html_safe
+    end
+  end
 end
