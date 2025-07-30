79606a8fd3f86ec643d38d65ceccff9ca4207e86
Ubuntu <jd069@hotmail.com>
2025-07-30 00:44:33 +0000

    Added study group invitations controller

---
+++ app/controllers/study_group_invitations_controller.rb
@@ -0,0 +1,37 @@
+class StudyGroupInvitationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_invitation, only: [:show, :accept, :decline]
+  
+  def index
+    @pending_invitations = current_user.pending_study_group_invitations
+                                      .includes(:study_group, :inviter)
+                                      .recent
+    @sent_invitations = current_user.sent_study_group_invitations
+                                   .includes(:study_group, :invitee)
+                                   .recent
+                                   .limit(20)
+  end
+  
+  def show
+  end
+  
+  def accept
+    if @invitation.accept!
+      redirect_to @invitation.study_group, notice: "Welcome to #{@invitation.study_group.name}! You've successfully joined the clan."
+    else
+      redirect_to study_group_invitations_path, alert: 'Unable to accept invitation. The group may be full.'
+    end
+  end
+  
+  def decline
+    @invitation.decline!
+    redirect_to study_group_invitations_path, notice: 'Invitation declined.'
+  end
+  
+  private
+  
+  def set_invitation
+    @invitation = current_user.received_study_group_invitations.find(params[:id])
+  end
+end
+
